1219af50973de25c4365959e80f9e8f87e6219d2
BumboBlast <john.jameschar@gmail.com>
2023-05-06 14:26:28 -0400

    Gave a padding around the game frame

--- src/Layout.py
+++ src/Layout.py
@@ -20,11 +20,14 @@ class Layout:
 
         """ At the moment, the sizes are relative to this empty corner.
         Later, want them to be relative to the clue_frames, so i can input a variable number of clues. """
-        corner_width = game_width * 0.3
+        corner_width = game_width * 0.25
         corner_height = corner_width
 
+        padding_width = corner_width * 0.1
+        padding_height = corner_height * 0.1
+
         horizontal_clues_width = corner_width
-        horizontal_clues_height = game_height - corner_height
+        horizontal_clues_height = game_height - corner_height - (2 * padding_height)
 
         vertical_clues_width = game_width - corner_width
         vertical_clues_height = corner_height
@@ -34,18 +37,34 @@ class Layout:
 
         # corner frame. Represents the empty space between the clues, complement of the picross board.
         self.corner_frame = Frame(self.game_frame, bg='light grey', width=corner_width, height=corner_height)
-        self.corner_frame.grid(row=0, column=0)
+        self.corner_frame.grid(row=1, column=1)
 
         # picross frame. Container holds the nonogram board.
         self.picross_frame = Frame(self.game_frame, bg='red', width=picross_width, height=picross_height)
-        self.picross_frame.grid(row=1, column=1)
+        self.picross_frame.grid(row=2, column=2)
 
         # horizontal clues frame. Container holds the horizontal clues. Rectangle to the left of the nonogram.
-        self.horizontal_clues = Frame(self.game_frame, bg='purple', width=horizontal_clues_width, height=horizontal_clues_height)
-        self.horizontal_clues.grid(row=1, column=0)
+        self.horizontal_clues = Frame(self.game_frame, bg='purple', width=horizontal_clues_width,
+                                      height=horizontal_clues_height)
+        self.horizontal_clues.grid(row=2, column=1)
 
         # # vertical clues frame. Container holds the vertical clues. Rectangle above the nonogram.
-        self.vertical_clues = Frame(self.game_frame, bg='light green', width=vertical_clues_width, height=vertical_clues_height)
-        self.vertical_clues.grid(row=0, column=1)
+        self.vertical_clues = Frame(self.game_frame, bg='light green', width=vertical_clues_width,
+                                    height=vertical_clues_height)
+        self.vertical_clues.grid(row=1, column=2)
 
+        # bottom buffer frame for padding
+        self.bottom_buffer = Frame(self.game_frame, bg='light pink', width=game_width, height=padding_height)
+        self.bottom_buffer.grid(row=3, column=1, columnspan=3)
 
+        # top buffer frame for padding
+        self.bottom_buffer = Frame(self.game_frame, bg='light pink', width=game_width, height=padding_height)
+        self.bottom_buffer.grid(row=0, column=1, columnspan=3)
+
+        # left buffer frame for padding
+        self.bottom_buffer = Frame(self.game_frame, bg='light pink', width=padding_width, height=game_height)
+        self.bottom_buffer.grid(row=0, column=0, rowspan=4)
+
+        # right buffer frame for padding
+        self.bottom_buffer = Frame(self.game_frame, bg='light pink', width=padding_width, height=game_height)
+        self.bottom_buffer.grid(row=0, column=3, rowspan=4)
