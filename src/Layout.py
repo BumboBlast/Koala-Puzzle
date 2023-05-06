43ed2ccd4f171b1e5757ec66d3c15474d29350a7
BumboBlast <john.jameschar@gmail.com>
2023-05-06 10:21:00 -0400

    Horizontal and Vertical Clue frames

--- src/Layout.py
+++ src/Layout.py
@@ -5,14 +5,47 @@ from tkinter import *
 
 class Layout:
     def __init__(self, root):
+        root.update_idletasks()
+
+        game_height = root.winfo_height() * 0.92
+        game_width = game_height
+
         # Main Frame. Container that holds the entire screen.
-        self.main_frame = Frame(root, bg='light blue')
-        self.main_frame.pack()
+        self.main_frame = Frame(root, bg='light blue', pady=(root.winfo_height() - game_height) / 2)
+        self.main_frame.place(x=0, y=0, width=root.winfo_width(), height=root.winfo_height())
+
+        # Game frame. Container that holds all UI elements
+        self.game_frame = LabelFrame(self.main_frame, text='Picross', bg='light pink', fg='black')
+        self.game_frame.pack()
+
+        """ At the moment, the sizes are relative to this empty corner.
+        Later, want them to be relative to the clue_frames, so i can input a variable number of clues. """
+        corner_width = game_width * 0.3
+        corner_height = corner_width
+
+        horizontal_clues_width = corner_width
+        horizontal_clues_height = game_height - corner_height
+
+        vertical_clues_width = game_width - corner_width
+        vertical_clues_height = corner_height
+
+        picross_width = vertical_clues_width
+        picross_height = horizontal_clues_height
+
+        # corner frame. Represents the empty space between the clues, complement of the picross board.
+        self.corner_frame = Frame(self.game_frame, bg='light grey', width=corner_width, height=corner_height)
+        self.corner_frame.grid(row=0, column=0)
+
+        # picross frame. Container holds the nonogram board.
+        self.picross_frame = Frame(self.game_frame, bg='red', width=picross_width, height=picross_height)
+        self.picross_frame.grid(row=1, column=1)
 
         # horizontal clues frame. Container holds the horizontal clues. Rectangle to the left of the nonogram.
-        self.horizontal_clues = Frame(self.main_frame, bg='pink', width=100, height=100)
-        self.horizontal_clues.pack(padx=10, pady=10)
+        self.horizontal_clues = Frame(self.game_frame, bg='purple', width=horizontal_clues_width, height=horizontal_clues_height)
+        self.horizontal_clues.grid(row=1, column=0)
 
-        # vertical clues frame. Container holds the vertical clues. Rectangle above the nonogram.
-        self.vertical_clues = Frame(self.main_frame, bg='light green', width=100, height=100)
-        self.vertical_clues.pack(padx=10, pady=10)
+        # # vertical clues frame. Container holds the vertical clues. Rectangle above the nonogram.
+        self.vertical_clues = Frame(self.game_frame, bg='light green', width=vertical_clues_width, height=vertical_clues_height)
+        self.vertical_clues.grid(row=0, column=1)
+
+
--- src/Main.py
+++ src/Main.py
@@ -3,7 +3,7 @@ from Game import *
 
 root = Tk()
 root.title('Koala Puzzle')
-root.geometry('1280x720')
+root.geometry('1440x900')
 root.bind('<Escape>', lambda kill: root.destroy())
 
 game = Game(root)
