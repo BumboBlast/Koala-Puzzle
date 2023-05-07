e4e2757b41f0936a055bbb0e71b653e026ece7f4
BumboBlast <john.jameschar@gmail.com>
2023-05-06 22:23:33 -0400

    began notation class

--- src/Game.py
+++ src/Game.py
@@ -9,6 +9,15 @@ class Game:
 
         layout = Layout(root)
 
+        # nonogram notation 1.
+        '2, 1,. 2, 1, 3,. 7,. 1, 3,. 2, 1... 2,. 2, 1,. 1, 1,. 3,. 1, 1,. 1, 1,. 2,. 1, 1,. 1, 2,. 2, ...'
+
+        """ each value ends with a comma. Each group (column or row) ends with a period. 
+            All COLUMNS are listed first, and then an ellipses (three periods), then all ROWS are listed 
+            End with three periods. """
+
+
+
         # make the buttons
         # for a in range(10):
         #     for b in range(10):
---
+++ src/Notation.py
@@ -0,0 +1,14 @@
+
+
+class Notation:
+
+    def __init__(self, notation):
+        self.notation = notation
+
+    def all_columns(self):
+        """ return a list of all columns. From the notation. """
+        pass
+
+    def all_rows(self):
+        """return a list of all rows. From the notation. """
+        pass
